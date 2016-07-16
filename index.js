144e7d1758141e10e95a779de6809c26cbb79d29
t ddddddd <webdev@cmme.org>
2016-07-16 16:20:37 -0700

    awesome express app

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+app.get('/', function(req, res) {
+  res.send('LLAMAS\n');
+});
+
+const PORT = 3000;
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
