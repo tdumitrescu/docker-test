b3e8bf26fed5603379806e1c9b627a85de4780a2
t ddddddd <webdev@cmme.org>
2016-07-17 22:58:43 -0700

    dynamic app response

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', function(req, res) {
-  res.send('LLAMAS\n');
+  res.send(`LLAMAS: ${Date()}\n`);
 });
 
 const PORT = 3000;
